52fc89d2f1da7af5bf13e291cf722b3a6265965d
vasih <vasih@DESKTOP-HG7JSRT>
2021-05-26 20:03:36 +0530

    react fragment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { Component } from 'react';
-import './App.css';
+import React, { Fragment, Component } from "react";
+import "./App.css";
 
 // function App() {
 //   return (
@@ -10,11 +10,16 @@ import './App.css';
 // }
 
 class App extends Component {
-  render(){return (
-    <div className="App">
-      <h1>hello it's class Component</h1>
-    </div>
-  );}
+  render() {
+    return (
+      // <div className="App">
+      <>
+        <h1>hello it's class Componenasdt</h1>
+        <h1>hello it's class Component</h1>
+      </> // react fragment
+      // </div>
+    );
+  }
 }
 
 export default App;
