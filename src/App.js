eeab671b2c6a64007a19f9722d213a7219709dcc
vasih <vasih@DESKTOP-HG7JSRT>
2021-05-26 19:47:17 +0530

    changed to class based component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>hello</h1>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render(){return (
     <div className="App">
-      <h1>hello</h1>
+      <h1>hello it's class Component</h1>
     </div>
-  );
+  );}
 }
 
 export default App;
